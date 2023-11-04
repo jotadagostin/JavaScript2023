f0f4f7b2ff8d8b1aa3425de4ecd20927a109680f
jotadagostin <107184368+jotadagostin@users.noreply.github.com>
2023-11-04 19:19:03 +0100

    js code
    class 7

---
+++ class6/index.js
@@ -0,0 +1,10 @@
+// let n1 = 1;
+// let n2 = "1";
+
+// let v1 = { nome: "Bruno" };
+// let v2 = v1;
+
+// console.log(v1 === v2);
+
+let nome = prompt("digite seu nome");
+alert("Olá, " + nome + "Como você está?");
---
+++ class7/index.js
@@ -0,0 +1,19 @@
+// logic operators
+// && and
+// || or
+// ! not
+
+let n1, n2, n3, n4;
+
+n1 = 10;
+n2 = 5;
+n3 = 15;
+n4 = 2;
+
+if ((n1>n2) && (n3>n4)) {
+    console.log("true");
+} else {
+    console.log("false");
+}
+
+// to return true both operations have to be true
