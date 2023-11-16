745e1d6f50f5bdae538106c8988b786d44240f2f
jotadagostin <107184368+jotadagostin@users.noreply.github.com>
2023-11-16 01:47:02 +0100

    Update index.js

--- class35/index.js
+++ class35/index.js
@@ -1,10 +1,32 @@
-const caixa1 = document.getElementById("#caixa1");
-const caixa2 = document.getElementById("#caixa2");
-const btn = document.getElementById("#btn_copiar");
-const todosCursos = [...document.querySelectorAll("curso")];
+const caixa1 = document.querySelector("#caixa1");
+const caixa2 = document.querySelector("#caixa2");
+const btn = document.querySelector("#btn_copiar");
+const todosCursos = [...document.querySelectorAll(".curso")];
+const cursosSelecionados = [...document.querySelectorAll(".selecionado")];
 
 todosCursos.map((elemento) => {
-    elemento.addEventListener("click", (evento) => {
-        const curso = evento.target
-    })
-})
+  elemento.addEventListener("click", (evento) => {
+    const curso = evento.target;
+    curso.classList.toggle("selecionado");
+  });
+});
+
+btn.addEventListener("click", () => {
+  const selecionados = [...document.querySelectorAll(".selecionado")];
+  selecionados.map((elemento) => {
+    caixa2.appendChild(elemento);
+  });
+});
+
+cursosSelecionados.map((elemento) => {
+  elemento.addEventListener("click", (evento) => {
+    const curso = evento.target;
+    curso.classList.remove("selecionado");
+  });
+});
+
+
+
+
+
+// toggle add and remove the classlist
