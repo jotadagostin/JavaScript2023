10ab36b103f9984675d82c9fb05109559e670db7
jotadagostin <107184368+jotadagostin@users.noreply.github.com>
2023-11-17 01:51:45 +0100

    javascript
    removing the itens

--- class35/index.js
+++ class35/index.js
@@ -1,8 +1,8 @@
 const caixa1 = document.querySelector("#caixa1");
 const caixa2 = document.querySelector("#caixa2");
-const btn = document.querySelector("#btn_copiar");
+const btn_transferir = document.querySelector("#btn_transferir");
 const todosCursos = [...document.querySelectorAll(".curso")];
-const cursosSelecionados = [...document.querySelectorAll(".selecionado")];
+
 
 todosCursos.map((elemento) => {
   elemento.addEventListener("click", (evento) => {
@@ -11,19 +11,18 @@ todosCursos.map((elemento) => {
   });
 });
 
-btn.addEventListener("click", () => {
+btn_transferir.addEventListener("click", () => {
   const selecionados = [...document.querySelectorAll(".selecionado")];
+  const naoSelecionados = [...document.querySelector(".curso:not(.selecionado")]
   selecionados.map((elemento) => {
     caixa2.appendChild(elemento);
   });
-});
-
-cursosSelecionados.map((elemento) => {
-  elemento.addEventListener("click", (evento) => {
-    const curso = evento.target;
-    curso.classList.remove("selecionado");
+  selecionados.map((elemento) => {
+    caixa1.appendChild(elemento);
   });
 });
+
+
 
 
 
