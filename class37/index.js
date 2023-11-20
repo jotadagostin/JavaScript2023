b81812b3a31841df30bc563980246517a0600359
jotadagostin <107184368+jotadagostin@users.noreply.github.com>
2023-11-20 12:12:02 +0100

    javascrip
    class 37  Elements DOM

---
+++ class37/index.js
@@ -0,0 +1,9 @@
+const caixa1 = document.querySelector("#caixa1");
+const btn_c = document.querySelectorAll(".curso");
+
+// console.log(caixa1.firstChild);
+// console.log(caixa1.lastChild);
+// console.log(caixa1.children);
+
+console.log(document.getRootNode());
+// raiz
