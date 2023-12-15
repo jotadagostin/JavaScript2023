049c0c75ed478dbb0ea7f2e1beb77c31ee083e8a
João Paulo Dagostin <dagostinjota@gmail.com>
2023-12-15 19:28:24 -0300

    Update index.js
    tamplate strings

--- class51/index.js
+++ class51/index.js
@@ -1,16 +1,22 @@
 const caixa = document.querySelector("#caixa");
 
-let musicas = new Set(["musica 1", "musica boa", "musica10"]);
+// let musicas = new Set(["musica 1", "musica boa", "musica10"]);
 
-musicas.add("msuica muito legal");
-musicas.add("musica1");
-musicas.add("musica1");
-musicas.add("musica10");
-musicas.delete("musica1");
-console.log(musicas);
+// musicas.add("msuica muito legal");
+// musicas.add("musica1");
+// musicas.add("musica1");
+// musicas.add("musica10");
+// musicas.delete("musica1");
+// console.log(musicas);
 
-// set nao permite entradas duplicadas, maior vantangem usando o set
+// // set nao permite entradas duplicadas, maior vantangem usando o set
 
-musicas.forEach((el) => {
-  caixa.innerHTML += el + "<br/> ";
-});
+// musicas.forEach((el) => {
+//   caixa.innerHTML += el + "<br/> ";
+// });
+
+const curso = "Javascript";
+const canal = "CFB Cursos";
+const frase = `Este e o curso de ${curso} do canal ${canal}`;
+
+caixa.innerHTML = frase;
