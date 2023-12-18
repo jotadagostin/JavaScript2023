0b44bacc4a3b7c9288131eef5c80301851d04a22
João Paulo Dagostin <dagostinjota@gmail.com>
2023-12-18 15:38:38 -0300

    js code
    starting learning  objects'

---
+++ class52/index.js
@@ -0,0 +1,15 @@
+// Objetos:
+
+class Pessoa {
+  constructor(pnome) {
+    this.nome = pnome;
+  }
+}
+
+let p1 = new Pessoa("Bruno");
+let p2 = new Pessoa("Ana");
+let p3 = new Pessoa("Cris");
+
+console.log(p1.nome);
+console.log(p2.nome);
+console.log(p3.nome);
