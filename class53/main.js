ea2cc54a3c79e27a08c660a1097973c955815bb7
João Paulo Dagostin <dagostinjota@gmail.com>
2023-12-19 15:03:53 -0300

    Update main.js
    objetos literais

--- class53/main.js
+++ class53/main.js
@@ -1,50 +1,78 @@
-// Objetos:
+// Objetos: literais
 
-function Pessoa(pnome, pidade) {
-  this.nome = pnome;
-  this.idade = pidade;
+// function Pessoa(pnome, pidade) {
+//   this.nome = pnome;
+//   this.idade = pidade;
 
-  (this.getNome = function () {
+//   (this.getNome = function () {
+//     return this.nome;
+//   }),
+//     (this.getidade = function () {
+//       return this.idade;
+//     }),
+//     (this.setNome = function (nome) {
+//       this.nome = nome;
+//     }),
+//     (this.setidade = function (idade) {
+//       this.idade = idade;
+//     }),
+//     (this.info = function () {
+//       console.log(`Nome: " + ${this.nome}`);
+//       console.log(`Idade: " + ${this.idade}`);
+//       console.log("------------------------");
+//     });
+// }
+
+// let pessoas = [];
+// const btn_add = document.querySelector("#btn_add");
+// const res = document.querySelector(".res");
+
+// const addPessoa = () => {
+//   res.innerHTML = "";
+//   pessoas.map((p) => {
+//     const div = document.createElement("div");
+//     div.setAttribute("class", "pessoa");
+//     div.innerHTML = `Nome: ${p.getNome()}<br/> Idade:${p.getidade()}`;
+//     res.appendChild(div);
+//   });
+// };
+
+// btn_add.addEventListener("click", (evt) => {
+//   const nome = document.querySelector("#f_nome");
+//   const idade = document.querySelector("#f_idade");
+//   const p = new Pessoa(nome.value, idade.value);
+//   pessoas.push(p);
+//   nome.value = "";
+//   idade.value = "";
+//   nome.focus();
+//   console.log(pessoas);
+//   addPessoa();
+// });
+
+const Pessoa = {
+  nome: "Bruno",
+  idade,
+  getNome: function () {
     return this.nome;
-  }),
-    (this.getidade = function () {
-      return this.idade;
-    }),
-    (this.setNome = function (nome) {
-      this.nome = nome;
-    }),
-    (this.setidade = function (idade) {
-      this.idade = idade;
-    }),
-    (this.info = function () {
-      console.log(`Nome: " + ${this.nome}`);
-      console.log(`Idade: " + ${this.idade}`);
-      console.log("------------------------");
-    });
-}
-
-let pessoas = [];
-const btn_add = document.querySelector("#btn_add");
-const res = document.querySelector(".res");
-
-const addPessoa = () => {
-  res.innerHTML = "";
-  pessoas.map((p) => {
-    const div = document.createElement("div");
-    div.setAttribute("class", "pessoa");
-    div.innerHTML = `Nome: ${p.getNome()}<br/> Idade:${p.getidade()}`;
-    res.appendChild(div);
-  });
+  },
+  getIdade: function () {
+    return this.idade;
+  },
+  setNome: function (nome) {
+    this.nome = nome;
+  },
+  setIdade: function (idade) {
+    this.idade = idade;
+  },
 };
 
-btn_add.addEventListener("click", (evt) => {
-  const nome = document.querySelector("#f_nome");
-  const idade = document.querySelector("#f_idade");
-  const p = new Pessoa(nome.value, idade.value);
-  pessoas.push(p);
-  nome.value = "";
-  idade.value = "";
-  nome.focus();
-  console.log(pessoas);
-  addPessoa();
-});
+const p2 = Pessoa;
+const p3 = Pessoa;
+
+p3.nome = "Clay";
+p2["nome"] = "Bridgs";
+Pessoa.setNome("Patryk");
+
+console.log(Pessoa.nome);
+console.log(p2.getNome());
+console.log(p3.nome);
