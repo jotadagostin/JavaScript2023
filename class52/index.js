7ca757aa1d3d446b733177f710ef42311750fb01
João Paulo Dagostin <dagostinjota@gmail.com>
2023-12-19 10:03:57 -0300

    js class code
    still aplying objects and functions

--- class52/index.js
+++ class52/index.js
@@ -1,15 +1,46 @@
 // Objetos:
 
-class Pessoa {
-  constructor(pnome) {
+class Carro {
+  constructor(pnome, ptipo) {
     this.nome = pnome;
+    if (ptipo == 1) {
+      this.tipo = "Esportivo";
+      this.velmax = 300;
+    } else if (ptipo == 2) {
+      this.tipo = "Utilitario";
+      this.velmax = 100;
+    } else if (ptipo == 3) {
+      this.tipo = "Passeio";
+      this.velmax = 160;
+    } else {
+      this.tipo = "Militar";
+      this.velmax = 180;
+    }
+  }
+  getNome() {
+    return this.nome;
+  }
+  getTipo() {
+    return this.tipo;
+  }
+  getvelmax() {
+    return this.velmax;
+  }
+  getInfo() {
+    return [this.nome, this.tipo, this.velmax];
+  }
+  info() {
+    console.log(`Nome: " + ${this.nome}`);
+    console.log(`Tipo: " + ${this.tipo}`);
+    console.log(`V.Max: " + ${this.velmax}`);
   }
 }
 
-let p1 = new Pessoa("Bruno");
-let p2 = new Pessoa("Ana");
-let p3 = new Pessoa("Cris");
+let c1 = new Carro("Rapidao", 1);
+let c2 = new Carro("Super Luxo", 2);
+let c3 = new Carro("Bombadao", 4);
+let c4 = new Carro("Carrego Tudo", 3);
 
-console.log(p1.nome);
-console.log(p2.nome);
-console.log(p3.nome);
+c1.info();
+c2.info();
+c3.info();
