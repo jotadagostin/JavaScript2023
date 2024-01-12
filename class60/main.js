b7b68eca10dc5513f26116043a6bc72a6cf7d509
João Paulo Dagostin <dagostinjota@gmail.com>
2024-01-12 18:49:16 -0300

    Update main.js
    setting the code for the calculator:
    
    sings and results

--- class60/main.js
+++ class60/main.js
@@ -4,19 +4,57 @@ const teclasRes = document.querySelector(".res");
 const display = document.querySelector(".display");
 const teclaOn = document.getAnimations("ton");
 const tLimpar = document.getElementById("tlimpar");
+const tIgual = document.getElementById("tigual");
+
+let sinal = false;
+let decimal = false;
 
 teclasNum.forEach((el) => {
   el.addEventListener("click", (evt) => {
-    display.innerHTML += evt.target.innerHTML;
+    sinal = false;
+    if (evt.target.innerHTML == ",") {
+      if (!decimal) {
+        decimal = true;
+        if (display.innerHTML == "0") {
+          display.innerHTML = "0,";
+        } else {
+          display.innerHTML += evt.target.innerHTML;
+        }
+      }
+    } else {
+      if (display.innerHTML == "0") {
+        display.innerHTML = "";
+      }
+      display.innerHTML += evt.target.innerHTML;
+    }
   });
 });
 
 teclasOp.forEach((el) => {
   el.addEventListener("click", (evt) => {
-    display.innerHTML += evt.target.innerHTML;
+    if (!sinal) {
+      sinal = true;
+      if (display.innerHTML == "0") {
+        display.innerHTML = "";
+      }
+      if (evt.target.innerHTML == "x") {
+        display.innerHTML += "*";
+      } else {
+        display.innerHTML += evt.target.innerHTML;
+      }
+    }
   });
 });
 
 tLimpar.addEventListener("click", (evt) => {
+  sinal = false;
+  decimal = false;
   display.innerHTML = 0;
+});
+
+tIgual.addEventListener("click", (evt) => {
+  sinal = false;
+  decimal = false;
+  const res = eval(display.innerHTML);
+  display.innerHTML = res;
 });
