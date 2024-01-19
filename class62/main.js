a90d8736426239c985b606ed4c9569d693aaee8b
João Paulo Dagostin <dagostinjota@gmail.com>
2024-01-19 10:44:16 -0300

    Update main.js
    creating a clock with js

--- class62/main.js
+++ class62/main.js
@@ -1,4 +1,5 @@
 const div_data = document.getElementById("div_data");
+const div_relogio = document.getElementById("div_relogio");
 const data = new Date();
 
 let dia = data.getDate();
@@ -10,6 +11,20 @@ mes = mes < 10 ? "0" + mes : mes;
 const data_r = dia + "/" + mes + "/" + data.getFullYear();
 
 div_data.innerHTML = data_r;
+
+const relogio = () => {
+  const data = new Date();
+  let hora = data.getHours();
+  hora = hora < 10 ? "0" + hora : hora;
+  let minuto = data.getMinutes();
+  minuto = minuto < 10 ? "0" + minuto : minuto;
+  let segundo = data.getSeconds();
+  segundo = segundo < 10 ? "0" + segundo : segundo;
+  const hora_completa = hora + ":" + minuto + ":" + segundo;
+  div_relogio.innerHTML = hora_completa;
+};
+
+const intervalo = setInterval(relogio, 1000);
 
 // getDate() = Dia do mês
 // getDay() = Dia da Semana (número)
