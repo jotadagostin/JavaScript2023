21c0fd4fbc34df20af709f7ed83b270febe4df25
João Paulo Dagostin <dagostinjota@gmail.com>
2024-01-20 16:45:36 -0300

    update js
    setting alarme code

--- class62/main.js
+++ class62/main.js
@@ -1,5 +1,41 @@
 const div_data = document.getElementById("div_data");
 const div_relogio = document.getElementById("div_relogio");
+const bnt_ativar = document.getElementById("#btn_ativar");
+const btn_parar = document.getElementById("btn_parar");
+const tmp_alarme = document.getElementById("tmp_alarme");
+const timer = document.getElementById("timer");
+
+const som_alarme = new Audio("alarme.mp3");
+som_alarme.loop = -1;
+
+let ts_atual = null; //timestamp(ts)
+let ts_alarme = null;
+let alarme_ativado = false;
+let alarme_tocando = false;
+
+bnt_ativar.addEventListener("click", () => {
+  ts_atual = Date.now();
+  ts_alarme = ts_atual + tmp_alarme.value * 1000;
+  alarme_ativado = true;
+  const dt_alarme = new Date(ts_alarme);
+  hora_alarme.innerHTML =
+    "Hora do Alarme:" +
+    dt_alarme.getHours() +
+    ":" +
+    dt_alarme.getMinutes() +
+    ":" +
+    dt_alarme.getSeconds();
+});
+btn_parar.addEventListener("click", () => {
+  alarme_ativado = false;
+  alarme_tocando = false;
+  hora_alarme.innerHTML = "Hora do Alarme:";
+  tmp_alarme.value = 0;
+  timer.classList.remove("alarme");
+  som_alarme.pause();
+  som_alarme.currentTime = 0;
+});
+
 const data = new Date();
 
 let dia = data.getDate();
@@ -22,6 +58,13 @@ const relogio = () => {
   segundo = segundo < 10 ? "0" + segundo : segundo;
   const hora_completa = hora + ":" + minuto + ":" + segundo;
   div_relogio.innerHTML = hora_completa;
+  if (alarme_ativado && !alarme_tocando) {
+    if (data.getTime() >= ts_alarme) {
+      alarme_tocando = true;
+      som_alarme.play();
+      timer.classList.add("alarme");
+    }
+  }
 };
 
 const intervalo = setInterval(relogio, 1000);
