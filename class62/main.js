598c1131b835bf4a61d9936f555a72a2fe06b389
João Paulo Dagostin <dagostinjota@gmail.com>
2024-01-21 16:19:17 -0300

    Update main.js
    fixing const

--- class62/main.js
+++ class62/main.js
@@ -1,6 +1,6 @@
 const div_data = document.getElementById("div_data");
 const div_relogio = document.getElementById("div_relogio");
-const bnt_ativar = document.getElementById("#btn_ativar");
+const bnt_ativar = document.getElementById("btn_ativar");
 const btn_parar = document.getElementById("btn_parar");
 const tmp_alarme = document.getElementById("tmp_alarme");
 const timer = document.getElementById("timer");
