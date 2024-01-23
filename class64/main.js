8fd42dc8f29302ce6689b9aea12d9fc7cafec0ee
João Paulo Dagostin <dagostinjota@gmail.com>
2024-01-23 11:00:23 -0300

    js code class
    setting the functions

--- class64/main.js
+++ class64/main.js
@@ -10,14 +10,53 @@ let bolas = [];
 let numBolas = 0;
 
 class Bola {
-    constructor() {
-        this.tam = Math.floor(Math.random()*15) + 10
-        this.r = Math.floor(Math.random()*255)
-        this.g = Math.floor(Math.random()*255)
-        this.b = Math.floor(Math.random()*255)
-        this.px= Math.floor(Math.random()*(larguraPalco-this.tam))
-        this.py = Math.floor(Math.random()*(alturaPalco-this.tam))
-    }
+  constructor(arrayBolas, palco) {
+    this.tam = Math.floor(Math.random() * 15) + 10;
+    this.r = Math.floor(Math.random() * 255);
+    this.g = Math.floor(Math.random() * 255);
+    this.b = Math.floor(Math.random() * 255);
+    this.px = Math.floor(Math.random() * (larguraPalco - this.tam));
+    this.py = Math.floor(Math.random() * (alturaPalco - this.tam));
+    this.velx = Math.floor(Math.random() * 2) + 0.5;
+    this.dirx = Math.random() * 10 > 5 ? 1 : -1;
+    this.diry = Math.random() * 10 > 5 ? 1 : -1;
+    this.palco = palco;
+    this.arrayBolas = arrayBolas;
+    this.id = Date.now() + "_" + Math.floor(Math.random() * 100000000000);
+    this.desenhar();
+    this.controle = setInterval(this.controlar, 10);
+    this.eu = document.getElementById(this.id);
+    numBolas++;
+    num_objetos.innerHTML = numBolas;
+  }
+  minhaPos = () => {
+    return this.arrayBolas.indexOf(this);
+  };
+
+  remover = () => {
+    clearInterval(this.controle);
+    bolas = bolas.filter((b) => {
+      if (b.id != this.id) {
+        return b;
+      }
+    });
+    this.eu.remove();
+    numBolas--;
+    num_objetos.innerHTML = numBolas;
+  };
+
+  desenhar = () => {
+    const div = document.createElement("div");
+    div.setAttribute("id", this.id);
+    div.setAttribute("class", "bola");
+    div.setAttribute(
+      "style",
+      `left:${this.px}px;top:${this.py}px;width:${this.tam}px;height:${this.tam}px;background-color:rgb(${this.r},${this.g},${this.b})`
+    );
+    this.palco.appendChild(div);
+  };
+
+  controlar = () => {};
 }
 
 window.addEventListener("resize", (evt) => {
@@ -25,14 +64,14 @@ window.addEventListener("resize", (evt) => {
   let alturaPalco = palco.offsetHeight;
 });
 
-btn_add.addEventListener("click", (evt) {
-    const qtde = txt_qtde.value 
-    for(let i = 0; i <  qtde; i++) {
-        //instanciar novas bolinhas
-    }
-})
-btn_remover.addEventListener("click", (evt) {
-    bolas.map((b) => {
-        //remover a bolinha
-    })
-})
+btn_add.addEventListener("click", (evt) => {
+  const qtde = txt_qtde.value;
+  for (let i = 0; i < qtde; i++) {
+    //instanciar novas bolinhas
+  }
+});
+btn_remover.addEventListener("click", (evt) => {
+  bolas.map((b) => {
+    //remover a bolinha
+  });
+});
--- class64/newFile.js
+++
@@ -1,3 +0,0 @@
-btn_remover.addEventListener("click", (evt) => {
-  bolas.map((b) => {});
-});
