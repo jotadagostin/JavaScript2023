8bb728cc07a0b9aeaf4afbbeb5962db12b962b47
João Paulo Dagostin <dagostinjota@gmail.com>
2024-02-14 20:54:50 -0300

    js class
    strings

---
+++ class73/main.js
@@ -0,0 +1,12 @@
+let nome = new String("bruno");
+let canal = new String("CFB Cursos");
+
+console.log(nome);
+console.log(typeof nome);
+
+console.log(nome.charAt(0));
+
+nome = nome.concat(canal);
+console.log(nome.concat(canal));
+
+console.log(nome.lastIndexOf("o"));
