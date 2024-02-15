492e31af0e48ffd7e39499690b69e39ea7e4eab8
João Paulo Dagostin <dagostinjota@gmail.com>
2024-02-14 21:26:36 -0300

    Update main.js
    strings and functions

--- class73/main.js
+++ class73/main.js
@@ -1,12 +1,35 @@
-let nome = new String("bruno");
+//functions para strings:
+
+let nome = new String("Bruno Santos");
+let nome2 = new String("Bruno Santos");
 let canal = new String("CFB Cursos");
 
-console.log(nome);
-console.log(typeof nome);
+// console.log(nome);
+// console.log(typeof nome);
 
-console.log(nome.charAt(0));
+// console.log(nome.charAt(0));
 
-nome = nome.concat(canal);
-console.log(nome.concat(canal));
+// nome = nome.concat(canal);
+// console.log(nome.concat(canal));
 
-console.log(nome.lastIndexOf("o"));
+// console.log(nome.lastIndexOf("o"));
+
+console.log(nome.localeCompare(nome2));
+// paracomparar strings
+
+console.log(nome.replace("B", "P"));
+console.log(nome.search("Santos"));
+console.log(nome.slice(3, 10));
+
+let arr_nome = nome.split("");
+console.log(arr_nome);
+
+//substring
+
+let parte1 = nome.substring(0, 5);
+let parte2 = nome.substr(6, 5);
+console.log(parte1.toUpperCase());
+console.log(parte2.valueOf());
+
+let num = 10;
+console.log(typeof num);
