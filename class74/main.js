1d20b058a7cf36f8c9657988d556b6aaea68cea2
João Paulo Dagostin <dagostinjota@gmail.com>
2024-02-19 19:21:32 -0300

    js class
    animation code

--- class74/main.js
+++ class74/main.js
@@ -1,19 +1,30 @@
 const carro = document.getElementById("carro");
 const btn_esquerda = document.getElementById("btn_esquerda");
 const btn_direita = document.getElementById("btn_direita");
+const btn_parar = document.getElementById("btn_parar");
 
 const init = () => {
   carro.style = "position: relative; left: 0px";
 };
 
+let anima = null;
+
+const move = (direcao) => {
+  carro.style.left = parseInt(carro.style.left) - 10 * direcao + "px";
+};
+
+btn_parar.addEventListener("click", () => {
+  clearInterval(anima);
+});
+
 btn_esquerda.addEventListener("click", () => {
-  carro.style.left = parseInt(carro.style.left) - 10 + "px";
+  clearInterval(anima);
+  anima = setInterval(move, 20, -1);
 });
 
 btn_direita.addEventListener("click", () => {
-  let position = parseInt(carro.style.left);
-  position += 10;
-  carro.style = `position: relative; left:${position}px`;
+  clearInterval(anima);
+  anima = setInterval(move, 20, 1);
 });
 
 window.onload = init;
