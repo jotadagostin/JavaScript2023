b22e8b528537f1117683cdbce1ea08b4cd6d1987
João Paulo Dagostin <dagostinjota@gmail.com>
2024-02-19 12:42:38 -0300

    Update main.js
    string class

--- class73/main.js
+++ class73/main.js
@@ -1,35 +1,12 @@
 //functions para strings:
 
-let nome = new String("Bruno Santos");
-let nome2 = new String("Bruno Santos");
-let canal = new String("CFB Cursos");
+let nome = new String(" Bruno Santos -");
 
-// console.log(nome);
-// console.log(typeof nome);
+console.log(nome.startsWith("Br"));
+console.log(nome.endsWith("os"));
+console.log(nome.includes("Br"));
+console.log(nome.repeat(4));
 
-// console.log(nome.charAt(0));
-
-// nome = nome.concat(canal);
-// console.log(nome.concat(canal));
-
-// console.log(nome.lastIndexOf("o"));
-
-console.log(nome.localeCompare(nome2));
-// paracomparar strings
-
-console.log(nome.replace("B", "P"));
-console.log(nome.search("Santos"));
-console.log(nome.slice(3, 10));
-
-let arr_nome = nome.split("");
-console.log(arr_nome);
-
-//substring
-
-let parte1 = nome.substring(0, 5);
-let parte2 = nome.substr(6, 5);
-console.log(parte1.toUpperCase());
-console.log(parte2.valueOf());
-
-let num = 10;
-console.log(typeof num);
+console.log(nome.charCodeAt(2));
+console.log(String.fromCodePoint(66));
+console.log(String.fromCodePoint(114));
