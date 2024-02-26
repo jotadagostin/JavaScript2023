1c71d29ea3073cf1786dfd1be1cbf2a1524e1a34
João Paulo Dagostin <dagostinjota@gmail.com>
2024-02-26 19:18:43 +0100

    main.js
    chanching the animation

--- class74/main.js
+++ class74/main.js
@@ -1,32 +1,52 @@
 const carro = document.getElementById("carro");
-const btn_esquerda = document.getElementById("btn_esquerda");
-const btn_direita = document.getElementById("btn_direita");
 const btn_parar = document.getElementById("btn_parar");
+const btn_rodar = document.getElementById("btn_rodar");
 
 const init = () => {
-  carro.style = "position: relative; left: 0px";
+  carro.style = "position:relative;left:0px;width:100px;height:40px";
+  tamCarro = parseInt(carro.style.width);
+  tamMax = window.innerWidth - tamCarro;
 };
 
 let anima = null;
+let tamMax = null;
+let tamCarro = null;
+let direcao = 0;
 
-const move = (direcao) => {
-  carro.style.left = parseInt(carro.style.left) - 10 * direcao + "px";
-  anima = setTimeout(move, 20, direcao);
+const move = () => {
+  if (parseInt(carro.style.left) >= tamMax) {
+    direcao = -1;
+  } else if (parseInt(carro.style.left) <= 0) {
+    direcao = 1;
+  }
+  carro.style.left = parseInt(carro.style.left) + 10 * direcao + "px";
+  anima = setTimeout(move, 20);
 };
 
 btn_parar.addEventListener("click", () => {
   clearTimeout(anima);
 });
 
-btn_esquerda.addEventListener("click", () => {
-  clearTimeout(anima);
-  move(-1);
+btn_rodar.addEventListener("click", () => {
+  move();
 });
 
-btn_direita.addEventListener("click", () => {
-  clearTimeout(anima);
-  move(1);
-});
+//window.onload = init;
+window.addEventListener("load", init());
+window,
+  addEventListener("resize", () => {
+    tamMax = window.innerWidth - parseInt(carro.style.width);
+  });
 
-window.onload = init;
-//window.addEventListener("load", init());
+window.addEventListener("keydown", (evt) => {
+  if (evt.code === "ArrowUp") {
+    carro.style.width = parseInt(carro.style.width) + 10 + "px";
+    carro.style.height = parseInt(carro.style.height) + 10 + "px";
+  }
+  if (evt.code === "ArrowDown") {
+    carro.style.width = parseInt(carro.style.width) - 10 + "px";
+    carro.style.height = parseInt(carro.style.height) - 10 + "px";
+  }
+  tamCarro = parseInt(carro.style.width);
+  tamMax = window.innerWidth - tamCarro;
+});
