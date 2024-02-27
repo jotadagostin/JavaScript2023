f1270c57b4699b6fae49b1f4e383cb74fd4bea6d
João Paulo Dagostin <dagostinjota@gmail.com>
2024-02-27 17:17:25 +0100

    Update meuPrimeiroProgarma.js

--- ALURAJS/meuPrimeiroProgarma.js
+++ ALURAJS/meuPrimeiroProgarma.js
@@ -1,23 +1,6 @@
-//myfirstprogram with js:
-
-const age = 26;
-console.log(age);
-
-//working with variables
-console.log("ano " + 2020);
-console.log(parseInt("2") + parseInt("2"));
-k;
-
-let idade = 25;
-let nome = "joao";
-let sobrenome = "Dagostin";
-
-console.log(nome, sobrenome);
-console.log(`Meu nome e:  ${nome} ${sobrenome}`);
-
-const NomeCompleto = nome + sobrenome;
-
 const listaDestino = new Array(`Salvador`, `Bahia`, `Rio de Janeiro`);
 
 listaDestino.push(`Curitiba`);
-listaDestino.splice();
+listaDestino.splice(2, 1);
+console.log(listaDestino);
+console.log(listaDestino[1]);
