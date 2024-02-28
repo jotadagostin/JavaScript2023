8cae5a3e11e532ff9cef66ea924f73fb26ddce61
João Paulo Dagostin <dagostinjota@gmail.com>
2024-02-28 20:54:32 +0100

    main.js
    js class code

--- ALURAJS/meuPrimeiroProgarma.js
+++ ALURAJS/meuPrimeiroProgarma.js
@@ -1,6 +1,39 @@
-const listaDestino = new Array(`Salvador`, `Bahia`, `Rio de Janeiro`);
+console.log(`TRABALHANDO COM CONDICIONAIS`);
 
-listaDestino.push(`Curitiba`);
-listaDestino.splice(2, 1);
-console.log(listaDestino);
-console.log(listaDestino[1]);
+const listaDeDestinos = new Array("Salvador", "Sao Paulo", "Rio de Janeiro");
+
+const idadeComprador = 18;
+const estaAcompanhada = false;
+let temPassagemComprada = false;
+const destino = "Sao Paulo";
+
+console.log("Destinos possiveis");
+console.log(listaDeDestinos);
+
+const podeComprar = idadeComprador >= 18 || estaAcompanhada == true;
+
+let contador = 0;
+let destinoExiste = false;
+
+while (contador > 3) {
+  if (listaDeDestinos[contador] == destino) {
+    console.log("Destino existe");
+    destinoExiste = true;
+    break;
+  }
+  contador += 1;
+}
+
+console.log("Destino existe: ", destinoExiste);
+
+if (podeComprar && destinoExiste) {
+  console.log("boa viagem");
+} else {
+  console.log("Desculpe, tivemos um erro");
+}
+
+for (let cont = 0; cont < 3; cont++) {
+  if (listaDeDestinos[contador] == destino) {
+    destinoExiste = true;
+  }
+}
