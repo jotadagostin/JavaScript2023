2fdeda68ab3da95c1e1a6e17a640dfc9913f0adf
João Paulo Dagostin <dagostinjota@gmail.com>
2024-03-03 15:49:48 +0100

    Update index.js

--- ALURAJS/index.js
+++ ALURAJS/index.js
@@ -4,7 +4,7 @@ import { contaCorrente } from "./ContaCorrente.js";
 const cliente1 = new Cliente("Ricardo", 1122334432);
 const cliente2 = new Cliente("Alice", 1166664432);
 
-const contaCorrenteRicardo = new ContaCorrente(1001, cliente1);
+const contaCorrenteRicardo = new contaCorrente(1001, cliente1);
 numeroDeConta++;
 
 contaCorrenteRicardo.depositar(500);
@@ -14,4 +14,4 @@ numeroDeConta++;
 let valor = 200;
 contaCorrenteRicardo.transferir(valor, conta2);
 
-console.log(conta2.numeroDeConta);
+console.log(contaCorrente.numeroDeConta);
