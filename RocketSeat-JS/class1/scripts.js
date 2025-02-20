549821efb07c51ab6b9d04fa69aeb234eeae0f79
João Paulo Dagostin <dagostinjota@gmail.com>
2025-02-20 10:25:44 -0300

    js class

--- RocketSeat-JS/class1/scripts.js
+++ RocketSeat-JS/class1/scripts.js
@@ -5,3 +5,27 @@
 
 let user;
 console.log("Paulo");
+
+//var com valor fixo: const, nao pode ser alterado
+
+const number = 42;
+console.log(number);
+
+//irar gerar um erro porque o valor nao pode ser alterado
+
+number = 55;
+console.l;
+
+//Escopo: define o contexto. Escopo Global e Bloco
+//Global: var disponivel em locais diferentes
+//Bloco: disponivel em so um local, naquele bloco.
+
+// nomes variaveis: case sensitive
+
+let username = "Jota";
+let userName = "Paulo";
+
+console.log(username);
+console.log(userName);
+
+let;
---
+++ RocketSeat-JS/class1/scripts2.js
@@ -0,0 +1,24 @@
+// tipos:
+
+//tipos primitivos: string, numer, boolean, null, undefined
+
+console.log('Uma string com "aspas duplas" dentro de uma simples');
+console.log("Uma string com 'aspas simples' dentro de uma aspa dupla");
+
+let username = "Jota";
+let email = "jota@gmail.com";
+console.log(username, email, "teste");
+
+// concatenar texto
+
+let message = "Ola, " + username + " . Voce conectou com o email: " + email;
+
+console.log(message);
+
+console.log();
+
+// number
+let number = 5;
+console.log(number);
+
+console.log(12.5 / 2);
