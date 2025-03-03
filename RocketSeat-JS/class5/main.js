4b81fba51b01419255652199ee0260a740d9f5d6
João Paulo Dagostin <dagostinjota@gmail.com>
2025-03-03 17:09:44 -0300

    js class

---
+++ RocketSeat-JS/class5/main.js
@@ -0,0 +1,20 @@
+//Vizualizar o conteudo do document
+console.log(document);
+
+//obter o title da pagina
+console.log(document.title);
+
+const guest = document.getElementById("guest-2");
+console.log(guest);
+
+console.dir();
+
+//acessar elemento com classs
+
+const guestsByclass = document.getElementsByClassName("guest");
+console.log(guestsByclass);
+console.log(guestsByclass.item(0));
+console.log(guestsByclass[1]);
+
+const guestTag = document.getElementsByTagName("li");
+console.log(guestTag);
