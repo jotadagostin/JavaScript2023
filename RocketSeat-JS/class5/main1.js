814ac4729362c97eef296c41ce1a06eae9ad19a9
João Paulo Dagostin <dagostinjota@gmail.com>
2025-03-04 17:21:14 -0300

    js class
    query selector

---
+++ RocketSeat-JS/class5/main1.js
@@ -0,0 +1,7 @@
+//acessa o elemento pelo seletor ID
+const guest5 = document.querySelector("#guest-1");
+console.log(guest5);
+
+// retorna todos os elementos encontrados pelo query
+const guest6 = document.querySelector(".guest");
+console.log(guest6);
