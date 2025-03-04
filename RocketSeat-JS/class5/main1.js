e484a53f3572c40cbe6abc4a27b2eb077f0a9ab8
João Paulo Dagostin <dagostinjota@gmail.com>
2025-03-04 19:45:44 -0300

    js class
    creating elements

--- RocketSeat-JS/class5/main1.js
+++ RocketSeat-JS/class5/main1.js
@@ -1,7 +1,16 @@
 //acessa o elemento pelo seletor ID
-const guest5 = document.querySelector("#guest-1");
-console.log(guest5);
+//const guest5 = document.querySelector("#guest-1");
+//console.log(guest5);
 
 // retorna todos os elementos encontrados pelo query
-const guest6 = document.querySelector(".guest");
-console.log(guest6);
+//const guest6 = document.querySelector(".guest");
+//console.log(guest6);
+//console.log("------------------------------------------------");
+
+const guestPRO = document.querySelector("#guest-1");
+console.log(guestPRO.textContent);
+
+guestPRO.textContent = "Jota";
+console.log(guestPRO.textContent); // retorna conteudo visivel e oculto
+console.log(guestPRO.innerHTML); // retorna o conteudo visivel
+console.log(guestPRO.innerText); // retorna o html como texto
---
+++ RocketSeat-JS/class5/main2.js
@@ -0,0 +1,28 @@
+//const input = document.querySelector("#name");
+
+//add
+//input.classList.add("input-error");
+
+//remove
+
+//input.classList.remove("input-error");
+
+//const button = document.querySelector("button");
+//button.style.backgroundColor = "red";
+
+const guests = document.querySelector("ul");
+
+const newGuests = document.createElement("li");
+newGuests.classList.add("guest");
+const guestName = document.createElement("span");
+
+guestName.textContent = "Diego";
+
+const newName = document.createElement("span");
+newName.textContent = "Fernandes";
+
+//add after the last child
+newGuests.append(guestName);
+newGuests.append(newName);
+
+guests.append(newGuests);
