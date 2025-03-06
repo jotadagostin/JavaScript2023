de7f6575f3c9695f0d9f35fdfc1e4bf14b329fad
João Paulo Dagostin <dagostinjota@gmail.com>
2025-03-06 10:50:18 -0300

    js class

---
+++ RocketSeat-JS/class5/main5.js
@@ -0,0 +1,22 @@
+const input = document.querySelector("input");
+
+//keydown: quando a tecla ah pressionada(captura tudo)
+
+/*input.addEventListener("keydown", (event) => {
+  console.log(event);
+});
+ */
+
+//keypres = dispara uma teclara do tipo caracter è pressionada
+
+input.addEventListener("keypress", (event) => {
+  console.log(event);
+});
+
+input.onchange = () => {
+  inputChange();
+};
+
+function inputChange() {
+  console.log("O input mudou");
+}
