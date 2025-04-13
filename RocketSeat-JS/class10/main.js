190d5d48f3e88709ea6d3a8233feea3448a78fd3
João Paulo Dagostin <dagostinjota@gmail.com>
2025-04-13 15:37:23 +0200

    js class

--- RocketSeat-JS/class10/main.js
+++ RocketSeat-JS/class10/main.js
@@ -98,3 +98,51 @@ console.log(adress); //prototype objeto
 
 const userProt = ["Joao", "Maria,", "Pedro"];
 console.log(userProt); //prototype array
+
+// classes para lidar com execoes mais especificas:
+
+let obj = [];
+let index = 300;
+
+try {
+  // obj.execute();
+  if (!obj.includes(15)) {
+    throw new Error("o numero 15 nao esta disponivel");
+  }
+
+  if (index > 99) {
+    throw new RangeError(
+      "numero esta fora do intervalo, escolha um numero de 0 a 99"
+    );
+  }
+} catch (error) {
+  if (error instanceof TypeError) {
+    console.log("metodo indisponivel");
+  }
+
+  if (error instanceof RangeError) {
+    console.log(error.message);
+  } else {
+    console.log("nao foi possivel realizar a acao!");
+  }
+}
+
+//como criar um error personalizado utilizando class:
+
+class MyCustomError {
+  constructor(message) {
+    this.message = "Class de erro customizada: " + message;
+  }
+}
+
+try {
+  // throw new Error("Erro generico");
+
+  throw new MyCustomError("Erro personalizado lancado!");
+} catch (error) {
+  if (error instanceof MyCustomError) {
+    console.log(error.message);
+  } else {
+    console.log("Nao foi possivel executar!");
+  }
+}
