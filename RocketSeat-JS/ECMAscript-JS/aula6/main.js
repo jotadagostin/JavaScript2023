c131d5799c9a3883281655edaceb91794842b139
João Paulo Dagostin <dagostinjota@gmail.com>
2025-05-16 10:54:03 +0200

    js class

--- RocketSeat-JS/ECMAscript-JS/aula6/main.js
+++ RocketSeat-JS/ECMAscript-JS/aula6/main.js
@@ -1,3 +1,42 @@
-const response = fetch("http://localhost:3333/products").then((response) => {
-  console.log(response);
+// const response = fetch("http://localhost:3333/products").then((response) => {
+//   response.json().then((data) => console.log(data));
+// });
+
+//utilizando o fetch com async/awai
+
+async function fetchProducts() {
+  const response = await fetch("http://localhost:3333/products");
+  const data = await response.json();
+  console.log(data);
+}
+
+// fetchProducts();
+
+async function fetchProductsById(id) {
+  const response = await fetch(`http://localhost:3333/products/${id}`);
+  const data = await response.json();
+  console.log(data);
+}
+// fetchProductsById("3");
+
+const productName = document.getElementById("name");
+const productPrice = document.getElementById("price");
+const form = document.getElementsByTagName("form");
+
+addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  fetch("http://localhost:3333/products", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      id: new Date().getTime().toString(),
+      name: productName.value,
+      price: productPrice.value,
+    }),
+  });
+
+  await fetchProducts();
 });
